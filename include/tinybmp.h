ec6c8f334f8b0894341b5f2b4fee3e8552100b81
Changbeom Choi <me@cbchoi.info>
2023-10-09 16:31:18 +0000

    tinybmp update

--- include/tinybmp.h
+++ include/tinybmp.h
@@ -5,19 +5,29 @@
 class tinybmp
 {
 public:
-	tinybmp();
+	tinybmp(int width, int height);
 	tinybmp(std::string file_name);
 	virtual ~tinybmp();
+	
 public:
 	void import_image(std::string file_name);
 	void export_image(std::string file_name);
 
 public:
+	unsigned char** get_image_buf() const;
+	
+protected:
 	unsigned char** create_bitmap_24bit(int height, int width);
-
+	
+public:
+	int get_width();
+	int get_height();
+	
 protected:
 	BITMAP_HEADER m_bh;
 	BITMAP_INFO_HEADER m_bih;
 	unsigned char** m_image_buf;
-
+	
+protected:
+	void init_bmp_headers(int width, int height);
 };
--- src/tinybmp.cpp
+++ src/tinybmp.cpp
@@ -2,11 +2,13 @@
 #include <fstream>
 #include "tinybmp.h"
 
-tinybmp::tinybmp()
+tinybmp::tinybmp(int width, int height)
 	:m_image_buf(nullptr)
 {
-	
+	init_bmp_headers(width, height);
+	create_bitmap_24bit(height, width);	
 }
+
 tinybmp::tinybmp(std::string file_name)
 		:m_image_buf(nullptr)
 {
@@ -28,12 +30,19 @@ void tinybmp::import_image(std::string file_name)
 	fin.open(file_name.c_str(), std::ios::binary);
 
 	fin.read((char*)&m_bh, sizeof(BITMAP_HEADER));
-	fin.read((char*)&m_bih, sizeof(BITMAP_INFO_HEADER));
-
 	if(m_bh.type !=0x4D42)
 	{
 	   fin.close();
 	   std::cerr << "[ERROR] tiny.bitmap::tiny_bitmap() Invalid Image File" <<std::endl;
+	   exit(1);
+	}
+	
+	fin.read((char*)&m_bih, sizeof(BITMAP_INFO_HEADER));
+	
+	if(m_bih.bits != 24)
+	{
+	   fin.close();
+	   std::cerr << "[ERROR] tiny.bitmap::tiny_bitmap() can handle 24bits BMP Image" <<std::endl;
 	   exit(1);
 	}
 	
@@ -67,13 +76,8 @@ void tinybmp::export_image(std::string file_name)//resize
 	char padding_buf[4] = {0,0,0,0};
 	for(int i=0 ; i<m_bih.height ; i++)
 	{
-		for(int j=0 ; j<m_bih.width *3 ; j+=3)
-		{
-			fout.write((char*)(m_image_buf[m_bih.height-1-i]+j), sizeof(unsigned char));
-			fout.write((char*)(m_image_buf[m_bih.height-1-i]+j+1), sizeof(unsigned char));
-			fout.write((char*)(m_image_buf[m_bih.height-1-i]+j+2), sizeof(unsigned char));
-		}
-
+		fout.write((char*)(m_image_buf[m_bih.height-1-i]), m_bih.width * 3);
+		
 		if((m_bih.width % 4) !=0)
 		{
 			fout.write(padding_buf, (m_bih.width % 4));
@@ -91,4 +95,41 @@ unsigned char** tinybmp::create_bitmap_24bit(int height, int width)
 	}
 
 	return image_buf;
+}
+
+unsigned char** tinybmp::get_image_buf() const
+{
+	return m_image_buf;
+}
+
+int tinybmp::get_width()
+{
+	if(m_image_buf != nullptr)
+		return m_bih.width;
+	else
+		return -1;
+}
+
+int tinybmp::get_height()
+{
+	if(m_image_buf != nullptr)
+		return m_bih.height;
+	else
+		return -1;
+}
+
+void tinybmp::init_bmp_headers(int width, int height)
+{
+	m_bih.size = 0x28;
+	m_bih.width = width;
+	m_bih.height = height;
+	m_bih.planes = 0x01;
+	m_bih.bits = 0x18;
+    m_bih.image_size = (m_bih.bits/8) * 3 * m_bih.width * m_bih.height;
+    m_bih.x_resolution = 96 * 39.3701;				
+    m_bih.y_resolution = 96 * 39.3701;	
+	
+	m_bh.type = 0x4d42;
+	m_bh.size = (m_bih.bits/8) * 3 * m_bih.width * m_bih.height + 0x26;
+	m_bh.offset = 0x36;
 }
